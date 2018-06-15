ea544bba7fb7a1d13a4fdfbf0df803b1b8208f31
johanna <djforero08@misena.edu.co>
2018-06-14 21:29:18 -0500

    se completa por fin el editar, se trabaja en la opcion eliminar

--- vistas/admin/ficha-v.php
+++ vistas/admin/ficha-v.php
@@ -2,7 +2,7 @@
 require('header.php');
 require("../../clases/ficha.php");
 
-class fichaModel
+class FichaModel
 {
 //variable privada pdo
     Private $PDO;
@@ -10,7 +10,7 @@ class fichaModel
     {
       try
       { 
-        $this->PDO = new PDO("mysql:host=localhost;port=3306;dbname=senasistencia;charset=utf8","root","");
+        $this->PDO = new PDO("mysql:host=localhost;port=3308;dbname=senasistencia;charset=utf8","root","");
         $this->PDO->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
         //echo "se conecto";//probar conexion
 
@@ -77,7 +77,19 @@ class fichaModel
       return $ficha;
     }
 
-   
+    public function consultarPrograma()//reemplazar "XNombretabla" por el nombre de la tabla que correponda
+    {
+      try
+      {
+        $consulta = "SELECT ID_Programa, Nombre_Programa FROM programa_formacion";
+        $objeto = $this->PDO->prepare($consulta);
+        $objeto->execute();
+        $programa = $objeto->fetchAll(PDO::FETCH_OBJ);
+        return $programa;
+      } catch (Exception $e) {
+        die($e->getMessage());
+      }
+    }
         
        
 
@@ -86,9 +98,10 @@ class fichaModel
 
 
 }
+//aca comienza el controlador
     try
       { 
-        $PDO = new PDO("mysql:host=localhost;port=3306;dbname=senasistencia;charset=utf8","root","");
+        $PDO = new PDO("mysql:host=localhost;port=3308;dbname=senasistencia;charset=utf8","root","");
         $PDO->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
         //echo "se conecto";//probar conexion
 
@@ -99,7 +112,7 @@ class fichaModel
       }
 
 $ficha = new Ficha();//reemplazar $programa y xNombreClase por el nombre de la clase
-
+$modelo = new FichaModel();
 
 if (isset($_REQUEST['ac']))
 {
@@ -119,7 +132,7 @@ if (isset($_REQUEST['ac']))
       $consulta = "INSERT INTO ficha (ID_Ficha,FK_Programa,Num_Ficha,Grupo_Ficha,Jornada_Ficha,Trimestre_Ficha,Estado_Ficha,FechaDeCreacion_Ficha) VALUES (?,?,?,?,?,?,?,?)";
       $objeto = $PDO->prepare($consulta);
       $objeto->execute(array( $ficha->__GET('id_ficha'),$ficha->__GET('FK_programa'),$ficha->__GET('num_ficha'),
-      $programa->__GET('grupo'), $ficha->__GET('jornada'),$ficha->__GET('trimestre'),
+      $ficha->__GET('grupo'), $ficha->__GET('jornada'),$ficha->__GET('trimestre'),
       $ficha->__GET('estado'),$ficha->__GET('fechaCreacion')));
       header("Location: ficha-v.php");//reenplazar por  xnombreArchivo el nombre del archivo de la vista
       
@@ -133,36 +146,49 @@ if (isset($_REQUEST['ac']))
       $ficha->__SET('trimestre',$_REQUEST['trimestre']);
       if(isset($_REQUEST['estado'])){$estado ='1';}else{ $estado ='0'; }
       $ficha->__SET('estado',$estado);
+
       $consulta = "UPDATE ficha SET FK_Programa = ? , Num_Ficha = ? , Grupo_Ficha = ? ,
-      Jornada_Ficha = ? , Trimestre_Ficha = ?, Trimestre_Ficha = ? WHERE ID_Ficha = ?";
+      Jornada_Ficha = ? , Trimestre_Ficha = ?, Estado_Ficha = ? WHERE ID_Ficha = ?";
       $objeto = $PDO->prepare($consulta);
-      $objeto->execute(array( $ficha->__GET('id_ficha'),$ficha->__GET('FK_programa'),$ficha->__GET('num_ficha'),
-      $ficha->__GET('grupo'),$ficha->__GET('jornada'),$ficha->__GET('estado')));
+      $objeto->execute(array( $ficha->__GET('FK_programa'),$ficha->__GET('num_ficha'),
+      $ficha->__GET('grupo'),$ficha->__GET('jornada'),$ficha->__GET('trimestre'),$ficha->__GET('estado'),$ficha->__GET('id_ficha')));
    
     header("Location: ficha-v.php");//reenplazar por  xnombreArchivo el nombre del archivo de la vista
 
     break;
     case 'editar':
       $consulta ="SELECT*FROM ficha WHERE ID_Ficha = ?";
-      $objeto = $this->PDO->prepare($consulta);
+      $objeto = $PDO->prepare($consulta);
       $objeto->execute(array($_REQUEST['id']));
       $fila= $objeto->fetch(PDO::FETCH_OBJ);
 
-            $ficha->__SET('id_ficha', $fila->ID_Ficha);//se llama el campo de la tabla que corresponda con el atributo de la clase
+          /*  $ficha->__SET('id_ficha', $fila->ID_Ficha);//se llama el campo de la tabla que corresponda con el atributo de la clase
             $ficha->__SET('FK_programa', $fila->FK_Programa);//se llama el campo de la tabla que corresponda con el atributo de la clase
             $ficha->__SET('num_ficha',$fila->Num_Ficha);//se repite segun los campos que hayan en la tabla
             $ficha->__SET('grupo',$fila->Grupo_Ficha);//se repite segun los campos que hayan en la tabla
             $ficha->__SET('jornada',$fila->Jornada_Ficha);//se repite segun los campos que hayan en la tabla
             $ficha->__SET('trimestre',$fila->Trimestre_Ficha);//se repite segun los campos que hayan en la tabla
             $ficha->__SET('estado',$fila->Estado_Ficha);//se repite segun los campos que hayan en la tabla
-            $ficha->__SET('fechaCreacion',$fila->FechaDeCreacion_Ficha);
+            $ficha->__SET('fechaCreacion',$fila->FechaDeCreacion_Ficha);*/
+      $ficha = $modelo->editar($_REQUEST['id']);
 
-
-      //header("Location: programa-v.php");
+      //header("Location: ficha-v.php");
     break;
     case 'eliminar':
-      $modelo->eliminar($_REQUEST['id']);
-      header("Location: ../vistas/admin/ficha-v.php");
+      
+      echo "<script>var confi = confirm('¿seguro que desea eliminar el registro?'); 
+      </script>";
+       $confirma = "<script> document.write(confi) </script>";
+      echo $confirma;
+     /* if($confirma == false){
+        echo "falso";
+      }
+      else{
+        echo "verdad :)";
+      }
+      */
+      //$modelo->eliminar($_REQUEST['id']);
+      //header("Location: ../vistas/admin/ficha-v.php");
     break;
     default:
 
@@ -184,8 +210,19 @@ if (isset($_REQUEST['ac']))
                 <div class="divider"></div>
                 <div class="section">
                   <input type="hidden" name="id" value="<?php echo $ficha->__GET('id_ficha');?>" />
-                  <input type="text" name="num_ficha" placeholder="Fichas" value="<?php echo $ficha->__GET('num_ficha');?>" />
-                  <input type="text" name="FK_programa" placeholder="Programa" value="<?php echo $ficha->__GET('FK_programa');?>" />
+                  <input type="number" name="num_ficha" placeholder="Fichas" value="<?php echo $ficha->__GET('num_ficha');?>" />
+                  
+                  <label>Programa:</label>
+                  <select name="FK_programa">
+                      <?php 
+                      $laconsulta = $modelo->consultarPrograma();
+                      foreach ($laconsulta as $datos) {;?>
+
+                        <option value="<?php echo $datos->ID_Programa ;?>" <?php echo $ficha->__GET('FK_programa') == $datos->ID_Programa ? 'selected' : ''; ?> > <?php echo $datos->Nombre_Programa  ;?> </option>
+
+                      <?php } ;?>
+
+                  </select>                  
                   <input type="text" name="grupo" placeholder="Grupo de la ficha" value="<?php echo $ficha->__GET('grupo');?>" />
                   <input type="text" name="jornada" placeholder="Jornada de la ficha" value="<?php echo $ficha->__GET('jornada');?>" />
                   <input type="text" name="trimestre" placeholder="trimestre de la ficha" value="<?php echo $ficha->__GET('trimestre');?>" />
@@ -218,7 +255,11 @@ if (isset($_REQUEST['ac']))
   <thead>
   <tr>
     <th class="oculto">id</th>
+    <th>Programa</th>
     <th>Fichas</th>
+    <th>Grupo</th>
+    <th>Jornada</th>
+    <th>Trimestre</th>
     <th>Estado</th>
     <th>Fecha</th>
     <th>editar</th>
@@ -258,10 +299,10 @@ foreach( $result as $fila){?>
     <td><?php echo $fila->__GET('fechaCreacion')?></td>
     
     <td>
-      <a href="#?ac=editar&id=<?php echo $fila->__GET('id_programa')?>" name="boton" class="waves-effect waves-blue btn-flat grey-text text-darken-1"><i class="material-icons">edit</i></a>
+      <a href="?ac=editar&id=<?php echo $fila->__GET('id_ficha')?>" name="boton" class="waves-effect waves-blue btn-flat grey-text text-darken-1"><i class="material-icons">edit</i></a>
     </td>
     <td>
-      <a href="#?ac=eliminar&id=<?php echo $fila->__GET('id_programa');?>" name="boton" class="waves-effect waves-red btn-flat"><i class="material-icons">delete</i></a>
+      <a href="?ac=eliminar&id=<?php echo $fila->__GET('id_ficha');?>" name="boton" class="waves-effect waves-red btn-flat"><i class="material-icons">delete</i></a>
     </td>
   </tr>
 <?php }?>
