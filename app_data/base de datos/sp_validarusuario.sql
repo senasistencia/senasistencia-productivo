f53bf229c6adc4ca7e80454bfded53faa9a76f49
senasistencia <pro.senasistencia@gmail.com>
2018-06-16 12:54:11 -0500

    Se actualiza el procedimiento almacenado

--- app_data/base de datos/sp_validarusuario.sql
+++ app_data/base de datos/sp_validarusuario.sql
@@ -8,10 +8,14 @@
 #
 
 DROP PROCEDURE IF EXISTS `sp_validarUsuario`;
-CREATE PROCEDURE `sp_validarUsuario`(IN _usuario BIGINT(16),IN _pass VARCHAR(100))
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_validarUsuario`(IN _usuario BIGINT(16),IN _pass VARCHAR(100))
 BEGIN
 
-SELECT PrimerNombre_Cliente,Tipo_Rol FROM cliente inner join cliente_rol inner join rol
-      on cliente.Documento_Cliente = cliente_rol.FK_DocCliente and rol.ID_Rol =  cliente_rol.FK_Rol 
-      WHERE Documento_Cliente = (select FK_DocCliente from usuario where usuario.FK_DocCliente = _usuario AND usuario.Password_Hash = SHA1(_pass));
+SELECT Documento_Cliente,PrimerNombre_Cliente,Tipo_Rol FROM cliente inner JOIN cliente_rol inner JOIN rol
+
+      ON cliente.Documento_Cliente = cliente_rol.FK_DocCliente AND rol.ID_Rol =  cliente_rol.FK_Rol 
+
+      WHERE Documento_Cliente = (SELECT FK_DocCliente FROM usuario WHERE usuario.FK_DocCliente = _usuario AND usuario.Password_Hash = SHA1(_pass));
+
 END;
+
