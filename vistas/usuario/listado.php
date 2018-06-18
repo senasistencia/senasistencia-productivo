8d06b333a23f73459d2bcb94162911297e234cc5
David Rugeles <rugeles.david@gmail.com>
2018-06-17 21:26:29 -0500

    se crea la vista de listador

--- vistas/usuario/listado.php
+++ vistas/usuario/listado.php
@@ -1,10 +1,20 @@
 <?php 
 require('header.php');
-require('../../modelos/ficha')
+//require('../../modelos/ficha');
+require('../../app_data/config.php');
+require('../../app_data/conexion.php');
+$nficha = $_GET['f'];
+$consulta = "CALL sp_listadoA(?)";//el nombre de la tabla
+$objeto = $PDO->prepare($consulta);
+$objeto->execute(array($nficha));
+$tabla = $objeto->fetchAll(PDO::FETCH_OBJ);
 ?>
 <div class="container">
   <div class="row">
     <h3 class="center-align">Listado de aprendices</h3><div class="divider"></div>
+    <?php foreach ($tabla as $datos) {
+        print_r($datos);
+    } ?>
   </div>
 </div>
 
