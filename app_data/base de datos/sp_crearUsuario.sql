2b01bf0c98b0a24578c602b2ed3a0e5bbd47ced8
senasistencia <pro.senasistencia@gmail.com>
2018-06-22 00:37:32 -0500

    se agrega el procedimiento almacenado crearUsuario

---
+++ app_data/base de datos/sp_crearUsuario.sql
@@ -0,0 +1,11 @@
+﻿#
+# Procedure "sp_crearUsuario"
+#
+
+DROP PROCEDURE IF EXISTS `sp_crearUsuario`;
+CREATE PROCEDURE `sp_crearUsuario`(IN _documento BIGINT(16),IN _pass VARCHAR(30))
+BEGIN
+
+INSERT INTO usuario VALUES (Null,_documento,SHA1(_pass),1,CURDATE(),NULL);
+
+END;
