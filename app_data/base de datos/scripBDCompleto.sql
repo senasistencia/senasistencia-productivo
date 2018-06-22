e42b187d5e4677378f008d40a04642207fa5b2b0
johanna <djforero08@misena.edu.co>
2018-06-21 21:02:39 -0500

    se agregan los procedimientos almacenados respectivos a la creacion de ficha y asociacion de instructor con ficha

--- app_data/base de datos/scripBDCompleto.sql
+++ app_data/base de datos/scripBDCompleto.sql
@@ -301,6 +301,32 @@ SELECT PrimerNombre_Aprendiz,PrimerApellido_Aprendiz FROM aprendiz
 WHERE Estado_aprendiz = 1 AND FK_Ficha = _idFicha;
 END;
 
+#
+# Procedure "sp_asocCliente_Ficha"
+#
+
+DROP PROCEDURE IF EXISTS `sp_asocCliente_Ficha`;
+CREATE PROCEDURE `sp_asocCliente_Ficha`(IN _doc BIGINT(16),IN _idficha BIGINT(16))
+BEGIN
+
+INSERT INTO cliente_ficha VALUES(_doc,_idficha); 
+
+END;
+
+#
+# Procedure "sp_guardarFicha"
+#
+
+DROP PROCEDURE IF EXISTS `sp_guardarFicha`;
+CREATE PROCEDURE `sp_guardarFicha`(IN _id BIGINT(16),IN _fkprog BIGINT(16),IN _nficha VARCHAR(30),IN _gru INT(11),IN _jorn VARCHAR(15),IN _tri INT(11),IN _est TINYINT(1),IN _fchC DATE,IN _fchI DATE)
+
+BEGIN
+
+INSERT INTO ficha VALUES(_id,_fkprog,_nficha,_gru,_jorn,_tri,_est,_fchC,_fchI); 
+
+SELECT LAST_INSERT_ID() as ultimo;
+
+END;
 
 
 
---
+++ app_data/base de datos/sp_asocCliente_Ficha.sql
@@ -0,0 +1,16 @@
+﻿# Host: 127.0.0.1:3308  (Version 5.5.5-10.1.30-MariaDB)
+# Date: 2018-06-21 20:58:10
+# Generator: MySQL-Front 6.0  (Build 2.20)
+
+
+#
+# Procedure "sp_asocCliente_Ficha"
+#
+
+DROP PROCEDURE IF EXISTS `sp_asocCliente_Ficha`;
+CREATE PROCEDURE `sp_asocCliente_Ficha`(IN _doc BIGINT(16),IN _idficha BIGINT(16))
+BEGIN
+
+INSERT INTO cliente_ficha VALUES(_doc,_idficha); 
+
+END;
