9dacb0c420713d79cde39b808347e62e24c4d02b
Johanna <djforero08@misena.edu.co>
2018-06-25 03:34:50 -0500

    se carga procedimientos almacenados actualizar contraseña y buscar ususario para el proceso de actualizar contraseñas

---
+++ app_data/base de datos/sp_Actcontrasena.sql
@@ -0,0 +1,14 @@
+﻿# Host: 127.0.0.1  (Version 5.5.5-10.1.22-MariaDB)
+# Date: 2018-06-25 03:33:06
+# Generator: MySQL-Front 6.0  (Build 2.20)
+
+
+#
+# Procedure "sp_Actcontrasena"
+#
+
+DROP PROCEDURE IF EXISTS `sp_Actcontrasena`;
+CREATE PROCEDURE `sp_Actcontrasena`(IN _documento BIGINT(16),IN _pass VARCHAR(100))
+BEGIN
+UPDATE usuario SET Password_Hash = SHA1(_pass) WHERE _documento;
+END;
---
+++ app_data/base de datos/sp_buscarUsuario.sql
@@ -0,0 +1,15 @@
+﻿# Host: 127.0.0.1  (Version 5.5.5-10.1.22-MariaDB)
+# Date: 2018-06-25 03:32:38
+# Generator: MySQL-Front 6.0  (Build 2.20)
+
+
+#
+# Procedure "sp_buscarUsuario"
+#
+
+DROP PROCEDURE IF EXISTS `sp_buscarUsuario`;
+CREATE PROCEDURE `sp_buscarUsuario`()
+BEGIN
+SELECT Documento_Cliente,PrimerNombre_Cliente,SegundoNombre_Cliente,PrimerApellido_Cliente,Password_Hash FROM cliente inner join usuario
+on `Documento_Cliente`= `FK_DocCliente`;
+END;
